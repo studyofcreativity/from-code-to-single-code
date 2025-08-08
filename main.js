ce27df17a47ac04a6c86b6472ad415c22a698e96
studyofcreativity <study.of.creativity@gmail.com>
2025-08-07 21:08:08 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-function convert() {
-  const code = document.getElementById("inputCode").value;
-  const output = encodeToSpecial(code);
-  document.getElementById("outputCode").value = output;
+function encodeCode() {
+  const input = document.getElementById("inputCode").value;
+  const output = document.getElementById("outputCode");
+  output.value = encodeToSpecial(input);
+}
+
+function decodeCode() {
+  const input = document.getElementById("inputCode").value;
+  const output = document.getElementById("outputCode");
+  output.value = decodeToNormal(input);
 }
