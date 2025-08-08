8182c1171c9e197ab4aa71814b922c98f3877ec6
studyofcreativity <study.of.creativity@gmail.com>
2025-08-07 21:08:34 -0400

    Update encoder.js

--- encoder.js
+++ encoder.js
@@ -1,29 +1,41 @@
-// Diccionario de comandos especiales (solo tú y ChatGPT conocen)
-const commandMap = {
-  'while': '/cycle(repitiendo para nada)',
-  'if': '/test(cuestionamiento mental)',
-  '==': '/equals(paradoja cuántica)',
-  'function': '/box(cajita mágica)',
-  'let': '/spawn(surgen ideas)',
-  'true': '/realidad(aceptada)',
-  'false': '/mentira(piadosa)',
-  'for': '/loop(espiral sin fin)',
-  'console.log': '/output(voz del vacío)',
-  'return': '/send(de vuelta al abismo)',
-  'null': '/empty(cero total)',
-  'undefined': '/nowhere(ni en sueños)',
-  'ѵw': '/noise(no existe)'  // Ejemplo personalizado
+const specialMap = {
+  "for": "✪☉",
+  "in": "↭➥",
+  "range": "⍬⬵",
+  "random": "⫷∽",
+  "import": "≡⩎",
+  "print": "⋌≜",
+  "if": "☊⚮",
+  "else": "✦⌖",
+  "elif": "✹⊷",
+  "def": "⧫≌",
+  "return": "↰⤹",
+  "True": "♛⥀",
+  "False": "⚑⌁",
+  "None": "∅◬",
+  "int": "⧥⇋",
+  "str": "✺↯",
+  "float": "⧈⧬",
+  "while": "⏃⎔",
+  "break": "⤠⊗",
+  "continue": "⇉⇭",
+  "class": "⌘⎇",
+  "with": "⤙⛏",
+  "as": "↭⧿",
+  "open": "⌦⊓",
+  "read": "✶⧴",
+  "write": "✍↠",
+  "list": "⇌⛶",
+  "dict": "⧣⇔",
+  "from": "⤄⇖",
+  "global": "⧨⧻"
 };
 
 function encodeToSpecial(code) {
   let encoded = code;
-
-  // Reemplazar frases más largas primero
-  const keys = Object.keys(commandMap).sort((a, b) => b.length - a.length);
-  for (const key of keys) {
-    const regex = new RegExp('\\b' + key + '\\b', 'g');
-    encoded = encoded.replace(regex, commandMap[key]);
+  for (const word in specialMap) {
+    const regex = new RegExp(`\\b${word}\\b`, 'g');
+    encoded = encoded.replace(regex, specialMap[word]);
   }
-
   return encoded;
 }
