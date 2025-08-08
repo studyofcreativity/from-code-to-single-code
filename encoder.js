e3cfd9a140f527ec7c5084bdc67cd908826c264a
studyofcreativity <study.of.creativity@gmail.com>
2025-08-07 21:26:31 -0400

    Update encoder.js

--- encoder.js
+++ encoder.js
@@ -3,17 +3,13 @@ function encodeCode(code, language) {
   if (!dict) return code;
 
   let encoded = code;
-
-  // Reemplazamos todas las palabras clave en el texto
   for (const [word, symbol] of Object.entries(dict)) {
     const regex = new RegExp(`\\b${escapeRegExp(word)}\\b`, 'g');
     encoded = encoded.replace(regex, symbol);
   }
-
   return encoded;
 }
 
-// Escapar caracteres especiales en regex
 function escapeRegExp(string) {
   return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
 }
