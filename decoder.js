471863f3ebb970688d20ef0fa318abd331aa005d
studyofcreativity <study.of.creativity@gmail.com>
2025-08-07 21:22:55 -0400

    Update decoder.js

--- decoder.js
+++ decoder.js
@@ -1,10 +1,14 @@
 function decodeCode(code) {
+  // Iteramos todos los diccionarios y hacemos la traducción inversa
   for (const dict of Object.values(dictionaries)) {
     for (const [word, symbol] of Object.entries(dict)) {
-      const regex = new RegExp(symbol.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'), 'g');
+      const regex = new RegExp(escapeRegExp(symbol), 'g');
       code = code.replace(regex, word);
     }
   }
-
   return code;
+}
+
+function escapeRegExp(string) {
+  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
 }
